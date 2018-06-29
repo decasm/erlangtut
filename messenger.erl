aa84c55c07e1bdf9216ad1105bd0fb0db2f2b55c
Devon Smith <smithde@oclc.org>
2018-06-29 16:00:44 -0400

    19 and 20

--- messenger.erl
+++ messenger.erl
@@ -56,7 +56,7 @@
 %%% Change the function below to return the name of the node where the
 %%% messenger server runs
 server_node() ->
-	messenger@node1.
+	messenger@localhost .
 
 %%% This is the server process for the "messenger"
 %%% the user list has the format [{ClientPid1, Name1},{ClientPid22, Name2},...]
